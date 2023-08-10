ef848484545d6c1387de249ed8db3f0feff12a39
Praveer Panghal <admin@spinoinc.com>
2023-08-10 16:16:32 -0400

    .

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const allowedOrigins = ['http://localhost:3000/', 'https://ojaarv-api.onrender.com/'];
+const allowedOrigins = ['http://127.0.0.1:5500','https://ojaarv.com/'];
 app.use(cors({
     origin: allowedOrigins
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', allowedOrigins);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.get('/contact', (req, res) => {
     res.send('Hello, Express Ojaarv!');
 });
