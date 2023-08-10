a190b6d8adb0d1ddb2aee7d2c701ec820ea1764a
Praveer Panghal <admin@spinoinc.com>
2023-08-10 16:22:18 -0400

    .

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.get('/contact', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
